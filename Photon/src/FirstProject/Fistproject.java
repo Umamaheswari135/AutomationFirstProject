a5fe78e879ac2519e2a43fc99170b3e3797bd435
mumam <mumam@LAPTOP-MOQVLOAH>
2023-06-25 15:53:54 +0530

    second commit

--- Photon/src/FirstProject/Fistproject.java
+++ Photon/src/FirstProject/Fistproject.java
@@ -16,6 +16,7 @@ public class Fistproject {
 		driver.get("https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm");
         driver.findElement(By.name("firstname")).sendKeys("Kandha shasti");
         driver.findElement(By.name("lastname")).sendKeys("Kavasam");
+        driver.quit();
 	}
 
 }
